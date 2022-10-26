875b2bd0cbc287d10e7845e25792fd9aef156c01
George Pollard <gpollard@microsoft.com>
2022-10-27 09:24:06 +1300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ for x in $BASEDIR/home/*; do
     ln -s "$x" "$HOME/.$(basename $x)"
 done
 
+mkdir -p ~/.omnisharp/
 echo '{"RoslynExtensionsOptions":{"enableDecompilationSupport":true}}' > ~/.omnisharp/omnisharp.json
