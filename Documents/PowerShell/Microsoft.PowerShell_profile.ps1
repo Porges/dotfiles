4043bf9ec461fd390c3ab02da40da837450560ba
George Pollard <gpollard@microsoft.com>
2026-03-10 21:59:39 +1300

    fix transient prompt load order

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -14,7 +14,6 @@ if ($host.Name -eq 'ConsoleHost')
     $env:STARSHIP_CONFIG = "$HOME/.config/starship.toml"
     function Invoke-Starship-TransientFunction { "`e[38;2;245;169;127m❯ " } # same as "peach" in starship config
     Invoke-Expression (&starship init powershell)
-    Enable-TransientPrompt
 
     Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
@@ -31,6 +30,7 @@ if ($host.Name -eq 'ConsoleHost')
     # imported last so it doesn't override the prompt
     # I only want it for completions
     Import-Module posh-git
+    Enable-TransientPrompt
 }
 
 # Assume programs print UTF-8 (default $OutputEncoding)
